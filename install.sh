27c963c978f5a22f43d9ab19752545760c0e06c0
Kristofers Solo <kristians.cagulis@gmail.com>
2022-05-11 19:03:07 +0300

    Added archlinux-keyring to instalation

--- install.sh
+++ install.sh
@@ -4,10 +4,12 @@ git clone https://aur.archlinux.org/paru
 cd paru
 makepkg -si
 cd ..
+rm -rf paru
 
+paru -S archlinux-keyring
 paru -Syu - < pkg-files/minimal.txt
 
 cp -r .config ~
 cp -r .local ~
-cp .zshenv ~
+ln -s ~/.config/.zshenv ~
 git clone https://github.com/streetturtle/awesome-wm-widgets ~/.config/awesome
